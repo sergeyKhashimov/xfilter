8e01e369ee3ced8179582e9bd340df05100e8f97
sergey <hashimov.sergey@gmail.com>
2017-07-06 23:03:49 +0300

    Created Domain extended ActiveRecord layer class

---
+++ behaviors/XFilterBehavior.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace sergey_h\xfilter;
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 7/6/17
+ * Time: 10:50 PM
+ */
+class XFilterBehavior
+{
+    const FLOAT = 'float';
+    const FLAG = 'flag';
+    const STRING = 'string';
+}
---
+++ configurator/XFilterDomain.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace sergey_h\xfilter;
+use yii\db\ActiveRecord;
+
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 7/6/17
+ * Time: 10:31 PM
+ */
+abstract class XFilterDomain extends ActiveRecord
+{
+    protected  $query;
+    protected  $fields;
+
+
+    /** sets array of fields need to filter
+     *
+     * @return mixed
+     */
+    abstract function configure();
+
+    /**
+     * @return mixed
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+}
